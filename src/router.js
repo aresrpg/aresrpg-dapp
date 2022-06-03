128ae58ab88b0b120c4a1a63ab0f6e348cf16e73
Sceat <fetch@sceat.xyz>
2022-06-03 21:07:39 +0300

    fix: typo

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import e404 from './views/404.vue';
-import home from './views/home/vue'
+import home from './views/home.vue';
 
 export default createRouter({
   history: createWebHistory(),
