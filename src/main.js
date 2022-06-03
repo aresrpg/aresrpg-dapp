1d0697d2173a2a86643667891b4270739144dc8d
Sceat <fetch@sceat.xyz>
2022-06-03 21:24:11 +0300

    refactor: use toasts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp, provide } from 'vue';
 import { registerSW } from 'virtual:pwa-register';
+import Toast, { useToast } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 import app from './app.vue';
 import router from './router.js';
@@ -9,19 +11,14 @@ console.log(
   'color: #1565C0;font-weight:bold;font-size:20px;'
 );
 
-const vue_app = createApp(app)
+const vue_app = createApp(app);
+const toast = useToast();
 
-vue_app
-.use(router)
-.mount('#app')
+vue_app.use(router).use(Toast).mount('#app');
 
 const updateSW = registerSW({
   onOfflineReady() {
-    ElNotification({
-      offset: 90,
-      type: 'info',
-      message: 'Umara is ready to work offline!',
-    });
+    toast('ready to work offline!');
   },
 });
 
