8df23657441fbc83635bd832b9a1cb6eeb49bcbc
Sceat <fetch@sceat.xyz>
2023-02-09 00:25:12 +0800

    fix: remove dotenv

--- netlify/src/env.js
+++ netlify/src/env.js
@@ -1,7 +1,4 @@
-import { config } from 'https://deno.land/x/dotenv@v3.2.0/mod.ts';
 import { decode } from 'https://deno.land/std@0.176.0/encoding/base64.ts';
-
-config();
 
 const {
   PORT: port = 3000,
