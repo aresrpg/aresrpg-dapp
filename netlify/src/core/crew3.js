6bdefb7f99ffeee854b3031777e509f9683919af
Sceat <fetch@sceat.xyz>
2023-02-09 02:42:49 +0800

    chore: format

--- netlify/src/core/crew3.js
+++ netlify/src/core/crew3.js
@@ -1,4 +1,4 @@
-import { CREW3_API_KEY } from "../env.js";
+import { CREW3_API_KEY } from "../env.js"
 
 function fetch_crew3(endpoint, options) {
   return fetch(`https://api.crew3.xyz/communities/aresrpg${endpoint}`, {
@@ -6,20 +6,20 @@ function fetch_crew3(endpoint, options) {
     headers: {
       "x-api-key": CREW3_API_KEY,
     },
-  }).then((result) => result.json());
+  }).then((result) => result.json())
 }
 
 export default {
   async get_user(discord_id) {
     const { level, rank, id } =
-      (await fetch_crew3(`/users?discordId=${discord_id}`)) ?? {};
-    if (!id) return {};
-    return { level, rank, id };
+      (await fetch_crew3(`/users?discordId=${discord_id}`)) ?? {}
+    if (!id) return {}
+    return { level, rank, id }
   },
   async get_quests(crew3_id) {
     const { data } = await fetch_crew3(
       `/claimed-quests?user_id=${crew3_id}&status=success`,
-    );
-    return data.map(({ reward }) => reward);
+    )
+    return data.map(({ reward }) => reward)
   },
-};
+}
--- netlify/src/core/discord.js
+++ netlify/src/core/discord.js
@@ -2,20 +2,20 @@ import {
   DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET,
   DISCORD_REDIRECT_URI,
-} from "../env.js";
-import database from "../database.js";
+} from "../env.js"
+import database from "../database.js"
 
-const ARESRPG_DISCORD_ID = "265104803531587584";
-const DISCORD_API = "https://discord.com/api";
-const ARESRPG_STAFF_ID = "311251749182767105";
+const ARESRPG_DISCORD_ID = "265104803531587584"
+const DISCORD_API = "https://discord.com/api"
+const ARESRPG_STAFF_ID = "311251749182767105"
 
 function fetch_discord(endpoint, options) {
   return fetch(`${DISCORD_API}/${endpoint}`, options)
     .then((response) => response.json())
     .then(({ message, ...result }) => {
-      if (message) throw new Error(message);
-      return result;
-    });
+      if (message) throw new Error(message)
+      return result
+    })
 }
 
 async function get_access_token(refresh_token) {
@@ -30,8 +30,8 @@ async function get_access_token(refresh_token) {
       client_id: DISCORD_CLIENT_ID,
       client_secret: DISCORD_CLIENT_SECRET,
     }),
-  });
-  return access_token;
+  })
+  return access_token
 }
 
 export async function fetch_user({
@@ -39,11 +39,11 @@ export async function fetch_user({
   expiration,
   refresh_token,
 } = {}) {
-  if (!unsure_access_token) return;
+  if (!unsure_access_token) return
 
   const access_token = expiration <= Date.now()
     ? await get_access_token(refresh_token)
-    : unsure_access_token;
+    : unsure_access_token
 
   const { id, username, discriminator, avatar } = await fetch_discord(
     "/users/@me",
@@ -52,7 +52,7 @@ export async function fetch_user({
         Authorization: `Bearer ${access_token}`,
       },
     },
-  );
+  )
 
   const { roles } = await fetch_discord(
     `/users/@me/guilds/${ARESRPG_DISCORD_ID}/member`,
@@ -61,14 +61,14 @@ export async function fetch_user({
         Authorization: `Bearer ${access_token}`,
       },
     },
-  );
+  )
 
-  const staff = roles.includes(ARESRPG_STAFF_ID);
-  return { id, username, discriminator, staff, avatar };
+  const staff = roles.includes(ARESRPG_STAFF_ID)
+  return { id, username, discriminator, staff, avatar }
 }
 
 export async function link({ code }, { uuid }) {
-  if (!code) throw "CODE_MISSING";
+  if (!code) throw "CODE_MISSING"
 
   const { access_token, refresh_token, expires_in } = await fetch_discord(
     `/oauth2/token`,
@@ -86,19 +86,19 @@ export async function link({ code }, { uuid }) {
         scope: "identify%20guilds.members.read",
       }),
     },
-  );
+  )
 
-  const expiration = Date.now() + expires_in * 1000 - 5000;
+  const expiration = Date.now() + expires_in * 1000 - 5000
   const discord_user = await fetch_user({
     access_token,
     refresh_token,
     expiration,
-  });
+  })
 
-  if (!discord_user?.id) throw "USER_NOT_FOUND";
-  if (await database.is_already_linked(discord_user.id)) throw "ALREADY_LINKED";
+  if (!discord_user?.id) throw "USER_NOT_FOUND"
+  if (await database.is_already_linked(discord_user.id)) throw "ALREADY_LINKED"
 
-  const minecraft_user = await database.pull(uuid);
+  const minecraft_user = await database.pull(uuid)
 
   await database.push(uuid, {
     ...minecraft_user,
@@ -109,21 +109,21 @@ export async function link({ code }, { uuid }) {
       expiration,
       last_update: Date.now(),
     },
-  });
+  })
 
-  return true;
+  return true
 }
 
 export async function unlink(_, { uuid }) {
-  const user = await database.pull(uuid);
-  const { id } = user?.discord;
+  const user = await database.pull(uuid)
+  const { id } = user?.discord
 
-  if (!id) throw "NOT_LINKED";
+  if (!id) throw "NOT_LINKED"
 
   await database.push(uuid, {
     ...user,
     discord: undefined,
-  });
+  })
 
-  return true;
+  return true
 }
