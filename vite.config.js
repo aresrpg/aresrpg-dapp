9e81f380c240722c8dbe28f366971f77e3d44302
Sceat <fetch@sceat.xyz>
2023-11-17 20:33:13 +0200

    feat: remove vercel plugin

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 import vueI18n from '@intlify/unplugin-vue-i18n/vite';
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vercel(),
     vue(),
     vueI18n(),
     VitePWA({
