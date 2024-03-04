7fdcbae22823bb062b3795672656249eb11fd0e3
Sceat <fetch@sceat.xyz>
2024-03-04 02:04:11 +0200

    refactor: improve structure

---
+++ src/views/tab-terrain-editor.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template lang="pug">
+.container
+  span Terrain Editor
+</template>
+
+<style lang="stylus" scoped>
+.container
+  display flex
+  flex-flow column nowrap
+  width 100%
+  height 100%
+  padding 2em
+  border-radius 12px
+  overflow hidden
+  position relative
+  color #ECF0F1
+  span
+    display flex
+    width 100%
+    height 100%
+    color #eee
+    flex-flow column nowrap
+    width 100%
+    justify-content center
+    align-items center
+</style>
