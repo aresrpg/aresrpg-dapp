9202c57413bd90a44ffc42f69e100087652f5bdb
Sceat <fetch@sceat.xyz>
2024-03-04 02:04:46 +0200

    feat: support sui wallet standard

---
+++ src/core/sui/wallet.js
@@ -0,0 +1,142 @@
+import { EventEmitter, on } from 'events';
+
+import { aiter } from 'iterator-helper';
+import {
+  getWallets,
+  isWalletWithRequiredFeatureSet,
+} from '@mysten/wallet-standard';
+
+export const wallet_emitter = new EventEmitter();
+const internal_emitter = new EventEmitter();
+
+const is_sui_wallet = wallet =>
+  isWalletWithRequiredFeatureSet(wallet, [
+    'sui:signAndExecuteTransactionBlock',
+    'sui:signPersonalMessage',
+    'sui:signTransactionBlock',
+  ]);
+
+export async function initialize_wallets(last_selected_wallet_name) {
+  aiter(on(internal_emitter, 'update'))
+    .map(([wallet]) => wallet)
+    .reduce((wallets, wallet) => {
+      const last_wallet = wallets[wallet.name];
+
+      if (!last_wallet) {
+        wallet_emitter.emit('wallet', wallet);
+        return { ...wallets, [wallet.name]: wallet };
+      }
+
+      const { accounts, chain } = wallet;
+
+      // if accounts are differents
+      if (accounts && accounts !== last_wallet.accounts)
+        Object.assign(last_wallet, { accounts });
+
+      if (chain && chain !== last_wallet.chain)
+        Object.assign(last_wallet, { chain });
+
+      wallet_emitter.emit('wallet', last_wallet);
+
+      return wallets;
+    }, {});
+
+  const wallets = getWallets()
+    .get()
+    .filter(is_sui_wallet)
+    .map(({ accounts, chains, features, icon, name, version }) => ({
+      accounts,
+      chains,
+      icon,
+      version,
+      features,
+      name,
+      async connect() {
+        const connected = await features['standard:connect'].connect();
+        internal_emitter.emit('update', {
+          accounts: connected.accounts.map(({ address }) => ({ address })),
+          // if present it will be the same for all accounts
+          chain: connected.accounts[0]?.chains?.[0],
+          name,
+        });
+        wallet_emitter.emit('switch_wallet', name);
+
+        return connected;
+      },
+      async disconnect() {
+        await features['standard:disconnect']?.disconnect();
+        wallet_emitter.emit('switch_wallet', null);
+      },
+      signAndExecuteTransactionBlock({
+        transactionBlock,
+        account: { address },
+      }) {
+        return features[
+          'sui:signAndExecuteTransactionBlock'
+        ].signAndExecuteTransactionBlock({
+          account: { address },
+          transactionBlock,
+        });
+      },
+      signPersonalMessage(msg) {
+        return features['sui:signPersonalMessage'].signPersonalMessage({
+          account: memory.selected_address,
+          message: new TextEncoder().encode(msg),
+        });
+      },
+      signTransactionBlock(...args) {
+        return features['sui:signTransactionBlock'].signTransactionBlock(
+          ...args,
+        );
+      },
+    }));
+
+  wallets.forEach(
+    ({
+      accounts,
+      chains,
+      icon,
+      name,
+      version,
+      features,
+      connect,
+      disconnect,
+      signAndExecuteTransactionBlock,
+      signPersonalMessage,
+      signTransactionBlock,
+    }) => {
+      internal_emitter.emit('update', {
+        accounts,
+        chain: chains[0],
+        icon,
+        name,
+        version,
+        connect,
+        disconnect,
+        signAndExecuteTransactionBlock,
+        signPersonalMessage,
+        signTransactionBlock,
+      });
+
+      features['standard:events'].on('change', ({ accounts, chains }) => {
+        if (accounts)
+          internal_emitter.emit('update', {
+            accounts: accounts.map(({ address }) => ({ address })),
+            // if present it will be the same for all accounts
+            chain: accounts[0].chains?.[0],
+            name,
+          });
+        if (chains)
+          internal_emitter.emit('update', {
+            name,
+            chain: chains[0],
+          });
+      });
+    },
+  );
+
+  if (last_selected_wallet_name) {
+    const wallet = wallets.find(w => w.name === last_selected_wallet_name);
+    if (wallet) await wallet.connect();
+  }
+}
