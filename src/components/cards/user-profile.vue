96b9fc3232daf4f9bb895c6e80c8ecbc2e15bdfd
Sceat <fetch@sceat.xyz>
2024-03-04 02:03:58 +0200

    feat: add profiles

---
+++ src/components/cards/user-profile.vue
@@ -0,0 +1,250 @@
+<i18n>
+  en:
+    edit: Edit
+    lock: Lock
+    unlock: Unlock
+    delete: Delete
+    send: Send
+    delete_desc: Are you sure you want to delete this profile?
+    cancel: Cancel
+    confirm: Confirm
+    invalid_address: Invalid Sui address
+    lock_desc: You're about to lock this profile to access it in game, you can unlock it at anytime when you're done playing
+    unlock_desc: You're about to unlock this profile and retrieve it in your wallet
+  fr:
+    edit: Modifier
+    lock: Verrouiller
+    unlock: Déverrouiller
+    delete: Supprimer
+    send: Envoyer
+    delete_desc: Êtes-vous sûr de vouloir supprimer ce profil ?
+    cancel: Annuler
+    confirm: Confirmer
+    invalid_address: Adresse Sui invalide
+    lock_desc: Vous êtes sur le point de verrouiller ce profil pour y accéder en jeu, vous pouvez le déverrouiller à tout moment lorsque vous avez fini de jouer
+    unlock_desc: Vous êtes sur le point de déverrouiller ce profil et de le récupérer dans votre portefeuille
+</i18n>
+
+<template lang="pug">
+.profile(:class="{ locked: props.locked }")
+  span.name {{ props.profile.name }}
+  .field
+    .title mastery
+    .value.mastery {{ props.profile.mastery }}
+  .field
+    .title id
+    a.value.id(:href="profile_explorer_link" target="_blank") {{ props.profile.id.slice(0, 24) }}...
+  .actions
+    vs-button(v-if="!props.locked" type="transparent" size="small" disabled color="#FFCA28") {{ t('edit') }}
+    vs-button(
+      v-if="!props.locked"
+      type="transparent"
+      size="small"
+      color="#EF5350"
+      @click="delete_dialog = true") {{ t('delete') }}
+    vs-button(v-if="!props.locked" type="transparent" size="small" color="#42A5F5" @click="send_dialog = true") {{ t('send') }}
+    vs-button(v-if="!props.locked" type="transparent" size="small" color="#4CAF50" @click="lock_dialog = true") {{ t('lock') }}
+    vs-button(v-else type="transparent" size="small" color="#4CAF50" @click="unlock_dialog = true") {{ t('unlock') }}
+
+    /// deletion dialog
+    vs-dialog(v-model="delete_dialog" :loading="delete_loading")
+      template(#header) {{ t('delete') }}
+      span {{ t('delete_desc') }}
+      template(#footer)
+        .dialog-footer
+          vs-button(type="transparent" color="#E74C3C" @click="delete_dialog = false") {{ t('cancel') }}
+          vs-button(type="transparent" color="#2ECC71" @click="delete_profile") {{ t('confirm') }}
+
+    /// lock dialog
+    vs-dialog(v-model="lock_dialog" :loading="lock_loading")
+      template(#header) {{ t('lock') }}
+      span {{ t('lock_desc') }}
+      template(#footer)
+        .dialog-footer
+          vs-button(type="transparent" color="#E74C3C" @click="lock_dialog = false") {{ t('cancel') }}
+          vs-button(type="transparent" color="#2ECC71" @click="lock_profile") {{ t('confirm') }}
+
+    /// unlock dialog
+    vs-dialog(v-model="unlock_dialog" :loading="unlock_loading")
+      template(#header) {{ t('unlock') }}
+      span {{ t('unlock_desc') }}
+      template(#footer)
+        .dialog-footer
+          vs-button(type="transparent" color="#E74C3C" @click="unlock_dialog = false") {{ t('cancel') }}
+          vs-button(type="transparent" color="#2ECC71" @click="unlock_profile") {{ t('confirm') }}
+
+    /// send dialog
+    vs-dialog(v-model="send_dialog" :loading="send_loading")
+      template(#header) {{ t('send') }}
+      .dialog-content
+        vs-input(v-model="send_to" block label="Sui address" color="#448AFF" icon-after)
+          template(#icon)
+            i.bx.bx-droplet
+          template(#message-warn v-if="send_to && !is_valid_sui_address") {{ t('invalid_address') }}
+      template(#footer)
+        .dialog-footer
+          vs-button(type="transparent" color="#E74C3C" @click="send_dialog = false") {{ t('cancel') }}
+          vs-button(type="transparent" color="#2ECC71" @click="send_profile") {{ t('confirm') }}
+</template>
+
+<script setup>
+import { computed, ref, inject } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { isValidSuiAddress } from '@mysten/sui.js/utils';
+
+import { use_client } from '../../core/sui/client';
+
+const { t } = useI18n();
+const props = defineProps(['profile', 'locked']);
+const client = use_client();
+
+const selected_wallet = inject('selected_wallet');
+
+const network = computed(() => {
+  const current_chain = selected_wallet.value?.chain;
+  if (!current_chain) return 'mainnet';
+
+  const [, chain] = current_chain.split(':');
+
+  return chain;
+});
+
+const profile_explorer_link = computed(
+  () => `https://suiscan.xyz/${network.value}/object/${props.profile.id}`,
+);
+
+const is_valid_sui_address = computed(() => {
+  const is_alias = send_to.value.endsWith('.sui');
+  return is_alias || isValidSuiAddress(send_to.value);
+});
+
+const delete_dialog = ref(false);
+const delete_loading = ref(false);
+
+const edit_dialog = ref(false);
+
+const lock_dialog = ref(false);
+const lock_loading = ref(false);
+
+const send_dialog = ref(false);
+const send_to = ref('');
+const send_loading = ref(false);
+
+const unlock_dialog = ref(false);
+const unlock_loading = ref(false);
+
+async function delete_profile() {
+  try {
+    delete_loading.value = true;
+    await client.delete_profile(props.profile.id);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    delete_loading.value = false;
+    delete_dialog.value = false;
+  }
+}
+
+async function lock_profile() {
+  try {
+    lock_loading.value = true;
+    const { storage_id, storage_cap_id } = await client.get_storage_id();
+
+    await client.lock_user_profile({
+      profile_id: props.profile.id,
+      storage_id,
+      storage_cap_id,
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    lock_loading.value = false;
+    lock_dialog.value = false;
+  }
+}
+
+async function unlock_profile() {
+  try {
+    unlock_loading.value = true;
+    const { storage_id, storage_cap_id } = await client.get_storage_id();
+
+    console.log('unlocking', storage_id, storage_cap_id, props.profile.id);
+
+    await client.unlock_user_profile({
+      profile_id: props.profile.id,
+      storage_id,
+      storage_cap_id,
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    unlock_loading.value = false;
+    unlock_dialog.value = false;
+  }
+}
+
+async function send_profile() {
+  try {
+    send_loading.value = true;
+    if (!is_valid_sui_address.value) throw new Error('Invalid Sui address');
+    await client.send_object(props.profile.id, send_to.value);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    send_loading.value = false;
+    send_dialog.value = false;
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.profile
+  width 300px
+  height max-content
+  backdrop-filter blur(10px)
+  background rgba(0, 0, 0, .3)
+  border-radius 10px
+  display flex
+  flex-flow column nowrap
+  padding .6em 1em 0
+  justify-content center
+  overflow hidden
+  &.locked
+    border 1px solid #FFCA28
+
+  .actions
+    display flex
+    justify-content flex-end
+    font-size .8em
+
+  span.name
+    text-transform capitalize
+    font-size 1.2em
+    color #ddd
+
+  .field
+    display flex
+    flex-flow row nowrap
+    align-items center
+    .title
+      font-weight bold
+      font-size .65em
+      text-transform uppercase
+      opacity .7
+    .value
+      font-size .8em
+      margin-left .5em
+
+    a
+      text-decoration underline
+      opacity .7
+      font-style italic
+
+.dialog-content
+  display flex
+  align-items center
+  justify-content center
+.dialog-footer
+  display flex
+  justify-content flex-end
+</style>
