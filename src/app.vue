0efabdfc8fad8c4fbff62a141ca7ba873e8c5a17
Sceat <fetch@sceat.xyz>
2024-03-09 23:36:39 +0200

    refactor: update main file

--- src/app.vue
+++ src/app.vue
@@ -3,13 +3,15 @@ router-view
 </template>
 
 <script setup>
-import { onMounted, provide, watch, ref, computed, nextTick } from 'vue';
+import { on } from 'events';
+
+import { onMounted, provide, watch, ref, reactive, onUnmounted } from 'vue';
 import { VsLoadingFn } from 'vuesax-alpha';
 import { useRouter } from 'vue-router';
-import { iter } from 'iterator-helper';
+import { aiter, iter } from 'iterator-helper';
 
 import { initialize_wallets, wallet_emitter } from './core/sui/wallet';
-import { get_alias, set_network } from './core/sui/client';
+import { get_alias, set_network, use_client } from './core/sui/client';
 
 const name = 'app';
 const router = useRouter();
@@ -19,10 +21,32 @@ const wallets = ref([]);
 const selected_wallet = ref(null);
 const selected_account = ref(null);
 const sidebar_reduced = ref(false);
+const resync = ref(0);
+
+const user = reactive({
+  has_shared_storage: false,
+  locked_profiles: null,
+  unlocked_profiles: null,
+  balance_sui: null,
+});
 
 const last_selected_address = localStorage.getItem('last_selected_address');
 
+let loading_instance = null;
+let resyncing_interval = null;
+
+provide('wallets', wallets);
+provide('selected_wallet', selected_wallet);
+provide('selected_account', selected_account);
+provide('loading', loading);
+provide('sidebar_reduced', sidebar_reduced);
+provide('user', user);
+
+const client = use_client(selected_wallet, selected_account);
+
+// ========================================
 // watch the selected wallet and select the first account if none is selected
+// ========================================
 watch(selected_wallet, () => {
   const wallet = selected_wallet.value;
   if (!wallet) return;
@@ -32,9 +56,92 @@ watch(selected_wallet, () => {
     wallet.accounts[0];
 });
 
-let loading_instance = null;
+async function update_sui_balance() {
+  try {
+    const balance = await client.get_sui_balance();
+    user.balance_sui = balance.toFixed(3);
+  } catch (error) {
+    console.error('Error while getting the Sui balance', error);
+  }
+}
+
+async function update_user_data() {
+  try {
+    const { storage_cap_id } = await client.get_storage_id();
+
+    user.unlocked_profiles = await client.get_unlocked_user_profiles();
+    if (storage_cap_id)
+      user.locked_profiles = await client.get_locked_profiles(storage_cap_id);
+  } catch (error) {
+    console.error('Error while updating the user data', error);
+  }
+}
+
+// ========================================
+// watch the selected account and populate the user object with blockchain data
+// ========================================
+watch(
+  selected_account,
+  async () => {
+    if (!selected_account.value) return;
+    loading.value++; // prevent interraction when switching accounts
+
+    user.unlocked_profiles = null;
+    user.locked_profiles = null;
+
+    // we want a non blocking subscription call
+    // it's currently unstable on Sui
+    client
+      .on_update()
+      .then(emitter => {
+        emitter.on('update', async event => {
+          await update_sui_balance();
+          await update_user_data();
+        });
+      })
+      .catch(error => {
+        console.error('Unable to subscribe to the Sui node', error);
+      });
+
+    await Promise.all([update_sui_balance(), update_user_data()]);
+    loading.value--;
+  },
+  { immediate: true },
+);
+
+watch(resync, () => {
+  if (!selected_account.value) return;
+
+  console.log('resyncing');
+  update_sui_balance();
+  update_user_data();
+});
+
+// ========================================
+// show the login animation if the loading ref is bigger than 0
+// ========================================
+watch(loading, value => {
+  if (value === 1) {
+    loading_instance?.close();
+    loading_instance = VsLoadingFn({
+      type: 'square',
+      color: '#42A5F5',
+      background: '#212121',
+    });
+  } else if (!value) {
+    loading_instance.close();
+  }
+});
+
+onUnmounted(() => {
+  loading_instance?.close();
+  clearInterval(resyncing_interval);
+});
 
 onMounted(async () => {
+  resyncing_interval = setInterval(() => resync.value++, 10000);
+
+  // ======= Handle Wallet Events =======
   wallet_emitter.on('wallet', async wallet => {
     try {
       // find the wallet by its name inside the wallets ref and replace it
@@ -84,26 +191,6 @@ onMounted(async () => {
 
   await initialize_wallets(localStorage.getItem('last_selected_wallet'));
 });
-
-watch(loading, value => {
-  if (value === 1) {
-    loading_instance?.close();
-    loading_instance = VsLoadingFn({
-      type: 'square',
-      color: '#42A5F5',
-      background: '#212121',
-    });
-  } else if (!value) {
-    loading_instance.close();
-  }
-});
-
-provide('resync', ref(0));
-provide('wallets', wallets);
-provide('selected_wallet', selected_wallet);
-provide('selected_account', selected_account);
-provide('loading', loading);
-provide('sidebar_reduced', sidebar_reduced);
 </script>
 
 <style lang="stylus">
@@ -134,6 +221,29 @@ sc-disableScollBar()
   i
     margin-right .5em
 
+.ares_btn
+  background rgba(#212121, .3)
+  backdrop-filter blur(12px)
+  padding 1em 2em
+  border-radius 3px
+  text-transform uppercase
+  font-weight 900
+  cursor pointer
+  border 1px solid rgba(black .4)
+  font-size .9em
+  text-shadow 0 0 3px black
+  color #eee
+  display flex
+  justify-content center
+  box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+  transition all 0.3s cubic-bezier(.25,.8,.25,1)
+  &:hover
+    box-shadow 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
+  &.disabled
+    opacity .5
+    cursor default
+    pointer-events none
+
 :root
   font-size 18px
   background #212121
@@ -160,4 +270,26 @@ sc-disableScollBar()
     :active
       color #e1c79b
       fill #e1c79b
+
+.material-1
+  box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+  transition all 0.3s cubic-bezier(.25,.8,.25,1)
+
+.material-1:hover
+  box-shadow 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
+
+.material-2
+  box-shadow 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)
+
+
+.material-3
+  box-shadow 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)
+
+
+.material-4
+  box-shadow 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)
+
+
+.material-5
+  box-shadow 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)
 </style>
