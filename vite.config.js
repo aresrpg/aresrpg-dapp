52262ea5de1835e1fa53eeb4ff28c36f63a1e433
Sceat <fetch@sceat.xyz>
2024-03-28 17:50:40 +0200

    fix: build target

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     vue(),
     vueI18n({}),
