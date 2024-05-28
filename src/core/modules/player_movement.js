5cb5b3c0e1b7003d830a002947e6f5309c1d1750
etienne <em85@pm.me>
2024-05-28 17:41:57 +0000

    fix: blocks cache issues

--- src/core/modules/player_movement.js
+++ src/core/modules/player_movement.js
@@ -66,8 +66,8 @@ export default function () {
       if (player.target_position) {
         const { x, z } = player.target_position
         const ground_pos = new Vector3(Math.floor(x), 0, Math.floor(z)) // .subScalar(0.5)
-        const ground_block = Heightmap.instance.getGroundBlock(ground_pos)
-        const ground_level = Math.floor(ground_block.pos.y)
+        Heightmap.instance.getGroundPos(ground_pos)
+        const ground_level = Math.floor(ground_pos.y)
         // hack: avoid player being stuck in terrain at spawning
         player.target_position.y = ground_level + 2
         player.move(player.target_position)
@@ -180,8 +180,8 @@ export default function () {
 
       const { x, z } = dummy.position
       const ground_pos = new Vector3(Math.floor(x), 0, Math.floor(z)) // .subScalar(0.5)
-      const ground_block = Heightmap.instance.getGroundBlock(ground_pos)
-      const ground_height = Math.floor(ground_block.pos.y)
+      Heightmap.instance.getGroundPos(ground_pos)
+      const ground_height = Math.floor(ground_pos.y)
 
       if (!ground_height) return
 
