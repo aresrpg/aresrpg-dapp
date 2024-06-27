857a16c3e26d6c9bec4fc96cd9791ae9d98b1811
KalvinVilla <villa.kalvin@gmail.com>
2024-06-27 23:01:02 +0200

    chore: improve ui

--- src/components/game-ui/game-stats.vue
+++ src/components/game-ui/game-stats.vue
@@ -6,8 +6,8 @@ en:
   intelligence: Intelligence
   vitality: Vitality
   chance: Chance
-  health_points: Health points (HP)
-  soul_points: Soul points (SP)
+  health_points: Health
+  soul_points: Soul
   ap: AP
   mp: MP
   points_to_distribute: Points to distribute
@@ -21,8 +21,8 @@ fr:
   intelligence: Intelligence
   vitality: Vitalité
   chance: Chance
-  health_points: Points de vie (PV)
-  soul_points: Etat de fantôme (SP)
+  health_points: Vie
+  soul_points: Âme
   ap: PA
   mp: PM
   points_to_distribute: Points à distribuer
@@ -45,48 +45,68 @@ fr:
     .experience-bar
       .label {{ t('experience') }}
       .bar
-        .fill(style={ width: `${percent_experience}%`, backgroundColor: 'blue' })
+        .fill(:style="{ width: `${percent_experience}%`, backgroundColor: 'blue' }")
+        .ticks
+          .tick(v-for="n in 5" :key="n" :style="{ left: `${n * 17.5}%` }")
     .health-bar
       .label {{ t('health_points') }}
       .bar
-        .fill(style={ width: `${percent_health}%` })
+        .fill(:style="{ width: `${percent_health}%` }")
+        .ticks
+          .tick(v-for="n in 4" :key="n" :style="{ left: `${n * 20}%` }")
     .soul-bar
       .label {{ t('soul_points') }}
       .bar
-        .fill(style={ width: `${percent_soul}%`, backgroundColor: 'red' })
+        .fill(:style="{ width: `${percent_soul}%`, backgroundColor: 'red' }")
+        .ticks
+          .tick(v-for="n in 4" :key="n" :style="{ left: `${n * 20}%` }")
   .content
     .stat
       .label {{ t('ap') }}
-      .value {{ 'N/A' }}
+      .value {{ pa }}
     .stat
       .label {{ t('mp') }}
-      .value {{ 'N/A' }}
+      .value {{ pm }}
     .stat
-      .label {{ t('vitality') }}
-      .value {{ stats.vitality }}
-      .increase-button
-        button(@click="increaseStat('vitality')") {{ t('increase') }}
+      .testlab
+        img(src="../../assets/statistics/vitality.png")
+        .label {{ t('vitality') }}
+      .value
+        .test {{ stats.vitality }}
+        .increase-button
+          button(@click="increaseStat('vitality')") {{ t('increase') }}
     .stat
-      .label {{ t('agility') }}
+      .testlab
+        img(src="../../assets/statistics/agility.png")
+        .label {{ t('agility') }}
       .value
         .test {{ stats.agility }}
         .increase-button
-        button(@click="increaseStat('agility')") {{ t('increase') }}
+          button(@click="increaseStat('agility')") {{ t('increase') }}
     .stat
-      .label {{ t('strength') }}
-      .value {{ stats.strength }}
-      .increase-button
-        button(@click="increaseStat('strength')") {{ t('increase') }}
+      .testlab
+        img(src="../../assets/statistics/strength.png")
+        .label {{ t('strength') }}
+      .value
+          .test {{ stats.strength }}
+          .increase-button
+            button(@click="increaseStat('strength')") {{ t('increase') }}
     .stat
-      .label {{ t('intelligence') }}
-      .value {{ stats.intelligence }}
-      .increase-button
-        button(@click="increaseStat('intelligence')") {{ t('increase') }}
+      .testlab
+        img(src="../../assets/statistics/intelligence.png")
+        .label {{ t('intelligence') }}
+      .value
+        .test {{ stats.intelligence }}
+        .increase-button
+          button(@click="increaseStat('intelligence')") {{ t('increase') }}
     .stat
-      .label {{ t('chance') }}
-      .value {{ stats.chance }}
-      .increase-button
-        button(@click="increaseStat('chance')") {{ t('increase') }}
+      .testlab
+        img(src="../../assets/statistics/chance.png")
+        .label {{ t('chance') }}
+      .value
+        .test {{ stats.chance }}
+        .increase-button
+          button(@click="increaseStat('chance')") {{ t('increase') }}
   .points-to-distribute
     .label {{ t('points_to_distribute') }}
     .value {{ available_points }}
@@ -112,13 +132,16 @@ const stats = ref([]);
 const name = ref('N/A');
 const level = ref('N/A');
 const classe = ref('N/A');
-const characterImage = ref('path/to/default/image.png'); // Chemin par défaut de l'image
+const characterImage = ref('https://assets.aresrpg.world/classe/iop_female.jpg');
 
 const health = ref(0);
 const max_health = ref(0);
 const soul_points = ref(0);
 const max_soul_points = ref(0);
 const available_points = ref(0);
+
+const pa = ref(12);
+const pm = ref(6);
 
 const percent_health = computed(() => {
   return Math.round((100 * health.value) / max_health.value);
@@ -161,7 +184,6 @@ function update_stats(state) {
 
   if (isNaN(max_health.value)) max_health.value = 30;
 
-  // Mettre à jour l'image du personnage si disponible
   if (character.image) {
     characterImage.value = character.image;
   }
@@ -176,21 +198,11 @@ onUnmounted(() => {
   context.events.off('STATE_UPDATED', update_stats);
 });
 
-// const pa = ref(12);
-// const pm = ref(6);
-
-// const percent_health = computed(() => {
-//   return Math.round((100 * health.value) / (max_health.value || 1));
-// });
-
-// console.log(percent_health);
-
 const increaseStat = (stat) => {
-  // Logique pour augmenter la statistique
   console.log(`Augmenter ${stat}`);
 };
 
-const { t } = useI18n(); // Assure-toi que useI18n est utilisé correctement
+const { t } = useI18n(); 
 </script>
 
 <style lang="stylus" scoped>
@@ -222,31 +234,49 @@ const { t } = useI18n(); // Assure-toi que useI18n est utilisé correctement
       .name
         font-size 1.5em
         font-weight bold
-        color green
       .classe-level
         display flex
         flex-direction column
         align-items center
         .classe, .level
-          font-size 1.2em
+          font-size 1em
   .bars
     display flex
     flex-direction column
     align-items center
     width 100%
-    margin-bottom 1em
+    margin-bottom 0.5em
     .experience-bar, .health-bar, .soul-bar
+      display flex
+      flex-direction row
+      justify-content space-between
       width 100%
-      border-radius 5px
       overflow hidden
+      margin-bottom 0.5em
       .label
-        font-size 1em
-        margin-bottom 0.5em
+        width 125px
+        font-size 0.8em
       .bar
+        border-radius 13px
         width 100%
-        height 10px
+        margin 0.2em
         background #777
+        position relative
+        .ticks
+          position absolute
+          top 0
+          left 0
+          width 100%
+          height 100%
+          .tick
+            position absolute
+            top 0
+            bottom 0
+            width 2px
+            background #fff
+            opacity 0.7
         .fill
+          border-radius 10px
           height 100%
           background #4caf50
   .content
@@ -257,20 +287,35 @@ const { t } = useI18n(); // Assure-toi que useI18n est utilisé correctement
       display flex
       justify-content space-between
       align-items center
-      padding .5em 0
+      padding-bottom .5em
+      .testlab
+        font-size 1em
+        vertical-align center
+        display flex
+        img
+          height 25px
+          width 25px
       .label
+        margin-left 0.2em
+        font-size 0.8em
         font-weight bold
+        align-content center
       .value
-        font-size 1.2em
-      .increase-button
-        button
-          margin-left 1em
-          padding 0.2em 0.5em
-          background #4caf50
-          color white
-          border none
-          border-radius 5px
-          cursor pointer
+        font-size 1em
+        vertical-align center
+        display flex
+        .test
+          align-content center
+        .increase-button
+          button
+            font-size 0.8em
+            margin-left 0.5em
+            padding 0.2em 0.5em
+            background #4caf50
+            color white
+            border none
+            border-radius 5px
+            cursor pointer
   .points-to-distribute
     display flex
     justify-content space-between
@@ -279,5 +324,8 @@ const { t } = useI18n(); // Assure-toi que useI18n est utilisé correctement
     .label
       font-weight bold
     .value
-      font-size 1.2em
+      margin-left 1em
+      color green
 </style>
+
+
