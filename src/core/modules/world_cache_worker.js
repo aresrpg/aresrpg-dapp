f3b7fb61102474f0a20edd505152b7f57b444ab2
etienne <em85@pm.me>
2024-07-03 09:37:26 +0000

    feat: use promise to notify when worker finished updating cache + code refactoring

--- src/core/modules/world_cache_worker.js
+++ src/core/modules/world_cache_worker.js
@@ -2,13 +2,107 @@ import { Biome, PatchCache, BlockType, Heightmap } from '@aresrpg/aresrpg-world'
 import { Box3, Vector3, MathUtils } from 'three'
 
 import {
-  blocks_colors,
   biome_mapping_conf,
   world_patch_size,
   world_cache_size,
 } from '../utils/terrain/world_settings.js'
 
 const dbg_highlight_patch_borders = false // debug only
+
+const fill_chunk = (chunk, block_pos, ground_type, buffer_over = []) => {
+  const { bbox, cache } = chunk
+  const dimensions = bbox.getSize(new Vector3())
+  const patch_size = dimensions.x
+
+  const level = MathUtils.clamp(
+    block_pos.y + buffer_over.length,
+    bbox.min.y,
+    bbox.max.y,
+  )
+  let buff_index = Math.max(level - block_pos.y, 0)
+  let h = level - bbox.min.y // local height
+  // debug_mode && is_edge(local_pos.z, local_pos.x, h, patch_size - 2)
+  //   ? BlockType.SAND
+  //   : block_cache.type
+
+  while (h >= 0) {
+    const cache_index =
+      block_pos.z * Math.pow(patch_size, 2) + h * patch_size + block_pos.x
+    const block_type = buff_index > 0 ? buffer_over[buff_index] : ground_type
+    const skip =
+      buff_index > 0 &&
+      cache[cache_index] !== undefined &&
+      !buffer_over[buff_index]
+    if (!skip) {
+      cache[cache_index] = block_type ? block_type + 1 : BlockType.NONE
+    }
+    buff_index--
+    h--
+  }
+}
+
+const add_edges_blocks = chunk => {
+  const { bbox } = chunk
+  const dimensions = bbox.getSize(new Vector3())
+  const patch_size = dimensions.x
+  for (let i = 0; i < patch_size; i++) {
+    const xmin = {
+      global_pos: new Vector3(bbox.min.x, 0, bbox.min.z + i),
+      local_pos: new Vector3(0, 0, i),
+    }
+    const xmax = {
+      global_pos: new Vector3(bbox.max.x, 0, bbox.min.z + i),
+      local_pos: new Vector3(patch_size - 1, 0, i),
+    }
+    const zmin = {
+      global_pos: new Vector3(bbox.min.x + i, 0, bbox.min.z),
+      local_pos: new Vector3(i, 0, 0),
+    }
+    const zmax = {
+      global_pos: new Vector3(bbox.min.x + i, 0, bbox.max.z),
+      local_pos: new Vector3(i, 0, patch_size - 1),
+    }
+    const edges = [xmin, zmin, xmax, zmax]
+    edges.forEach(edge => {
+      const block_data = PatchCache.getBlock(edge.global_pos)
+      const block_local_pos = edge.local_pos.clone()
+      block_local_pos.y = block_data.pos.y
+      fill_chunk(chunk, block_local_pos, block_data.type)
+      // else console.log('missing block: ', edge.pos)
+    })
+  }
+}
+
+const add_ground_blocks = (patch, chunk) => {
+  const iter = patch?.blockIterator(true)
+  let res = iter.next()
+  while (!res.done) {
+    const block_data = res.value
+    const block_pos = block_data.pos.clone()
+    block_pos.x += 1
+    block_pos.z += 1
+    let border_type
+    if (dbg_highlight_patch_borders) {
+      if (block_pos.x === 1 || block_pos.z === 1) {
+        border_type = patch.isTransitionPatch ? BlockType.SAND : BlockType.WATER
+        border_type = patch.isBiomeTransition ? BlockType.MUD : border_type
+      }
+    }
+    const block_type = border_type || block_data.type
+    fill_chunk(chunk, block_pos, block_type)
+    res = iter.next()
+  }
+}
+
+const add_entities_blocks = (patch, chunk) => {
+  // patch.spawned
+  const buff_iter = patch.overBlocksIter()
+  for (const blk of buff_iter) {
+    blk.localPos.x += 1
+    blk.localPos.z += 1
+    fill_chunk(chunk, blk.localPos, blk.type, blk.buffer)
+  }
+}
 
 const init_cache = () => {
   PatchCache.patchSize = world_patch_size
@@ -18,21 +112,11 @@ const init_cache = () => {
   // Biome (blocks mapping)
   Biome.instance.setMappings(biome_mapping_conf)
   Biome.instance.params.seaLevel = biome_mapping_conf.temperate.beach.x
-  PatchCache.updateCache(new Vector3(), world_cache_size / 4, true)
+  // PatchCache.updateCache(new Vector3(), world_cache_size / 4, true)
 }
 
-const update_cache = async player_position => {
-  const cache_refreshed = await PatchCache.updateCache(
-    player_position,
-    world_cache_size,
-  )
-  return { cacheRefreshed: cache_refreshed }
-}
-
-const get_ground_level = (x, z) => {
-  const block_pos = new Vector3(x, 0, z)
-  const ground_block = PatchCache.getBlock(block_pos)
-  return ground_block.pos.y
+const update_cache = player_position => {
+  return PatchCache.updateCache(player_position, world_cache_size)
 }
 
 const get_block = (x, z) => {
@@ -61,27 +145,12 @@ const get_block = (x, z) => {
   return block
 }
 
-const get_patch = (bmin, bmax) => {
-  const bbox = new Box3(
-    new Vector3(...Object.values(bmin)),
-    new Vector3(...Object.values(bmax)),
-  )
-  const patch = PatchCache.cache.find(
-    patch =>
-      patch.bbox.min.x === bbox.min.x &&
-      patch.bbox.min.z === bbox.min.z &&
-      patch.bbox.max.x === bbox.max.x &&
-      patch.bbox.max.z === bbox.max.z,
-  )
-  return patch?.blocks.level
-}
-
 const get_chunk = (block_start, block_end) => {
   // TODO make this function run in another thread
   const bbox = new Box3(block_start, block_end)
   const dimensions = bbox.getSize(new Vector3())
-  const patch_size = dimensions.x
   const cache = new Uint16Array(dimensions.x * dimensions.y * dimensions.z)
+  const chunk = { bbox, cache }
   // const debug_mode = true
 
   // const is_edge = (row, col, h, patch_size) =>
@@ -90,96 +159,6 @@ const get_chunk = (block_start, block_end) => {
   // || h === patch_size - 2
 
   let is_empty = true
-
-  const fill_blocks_struct = (block_pos, ground_type, buffer_over = []) => {
-    const level = MathUtils.clamp(
-      block_pos.y + buffer_over.length,
-      bbox.min.y,
-      bbox.max.y,
-    )
-    let buff_index = Math.max(level - block_pos.y, 0)
-    let h = level - bbox.min.y // local height
-    // debug_mode && is_edge(local_pos.z, local_pos.x, h, patch_size - 2)
-    //   ? BlockType.SAND
-    //   : block_cache.type
-
-    while (h >= 0) {
-      const cache_index =
-        block_pos.z * Math.pow(patch_size, 2) + h * patch_size + block_pos.x
-      const block_type = buff_index > 0 ? buffer_over[buff_index] : ground_type
-      const skip =
-        buff_index > 0 &&
-        cache[cache_index] !== undefined &&
-        !buffer_over[buff_index]
-      if (!skip) {
-        cache[cache_index] = block_type ? block_type + 1 : BlockType.NONE
-      }
-      buff_index--
-      h--
-    }
-  }
-
-  const add_ground_blocks = patch => {
-    const iter = patch?.blockIterator(true)
-    let res = iter.next()
-    while (!res.done) {
-      const block_data = res.value
-      const block_pos = block_data.pos.clone()
-      block_pos.x += 1
-      block_pos.z += 1
-      let border_type
-      if (dbg_highlight_patch_borders) {
-        if (block_pos.x === 1 || block_pos.z === 1) {
-          border_type = patch.isTransitionPatch
-            ? BlockType.SAND
-            : BlockType.WATER
-          border_type = patch.isBiomeTransition ? BlockType.MUD : border_type
-        }
-      }
-      const block_type = border_type || block_data.type
-      fill_blocks_struct(block_pos, block_type)
-      res = iter.next()
-    }
-  }
-
-  const add_entities_blocks = patch => {
-    // patch.spawned
-    const buff_iter = patch.overBlocksIter()
-    for (const blk of buff_iter) {
-      blk.localPos.x += 1
-      blk.localPos.z += 1
-      fill_blocks_struct(blk.localPos, blk.type, blk.buffer)
-    }
-  }
-
-  const add_edges_blocks = () => {
-    for (let i = 0; i < patch_size; i++) {
-      const xmin = {
-        global_pos: new Vector3(bbox.min.x, 0, bbox.min.z + i),
-        local_pos: new Vector3(0, 0, i),
-      }
-      const xmax = {
-        global_pos: new Vector3(bbox.max.x, 0, bbox.min.z + i),
-        local_pos: new Vector3(patch_size - 1, 0, i),
-      }
-      const zmin = {
-        global_pos: new Vector3(bbox.min.x + i, 0, bbox.min.z),
-        local_pos: new Vector3(i, 0, 0),
-      }
-      const zmax = {
-        global_pos: new Vector3(bbox.min.x + i, 0, bbox.max.z),
-        local_pos: new Vector3(i, 0, patch_size - 1),
-      }
-      const edges = [xmin, zmin, xmax, zmax]
-      edges.forEach(edge => {
-        const block_data = PatchCache.getBlock(edge.global_pos)
-        const block_local_pos = edge.local_pos.clone()
-        block_local_pos.y = block_data.pos.y
-        fill_blocks_struct(block_local_pos, block_data.type)
-        // else console.log('missing block: ', edge.pos)
-      })
-    }
-  }
 
   const patch = PatchCache.cache.find(
     patch =>
@@ -191,11 +170,11 @@ const get_chunk = (block_start, block_end) => {
   )
 
   if (patch) {
-    add_ground_blocks(patch)
+    add_ground_blocks(patch, chunk)
     // process entities
-    add_entities_blocks(patch)
+    add_entities_blocks(patch, chunk)
     // fill extra blocks at edges from adjacent patches
-    add_edges_blocks()
+    add_edges_blocks(chunk)
     is_empty = false
   }
   return cache
@@ -208,27 +187,27 @@ addEventListener('message', ({ data: input }) => {
     id: input.id,
   }
   switch (input.api) {
-    case 'getGroundLevel':
-      output.data = get_ground_level(...input.args)
-      postMessage(output)
-      break
     case 'getBlock':
       output.data = get_block(...input.args)
-      postMessage(output)
-      break
-    case 'getPatch':
-      output.data = get_patch(...input.args)
       postMessage(output)
       break
     case 'getChunk':
       output.data = get_chunk(...input.args)
       postMessage(output)
       break
-    case 'updateCache':
-      update_cache(...input.args).then(res => {
-        output.data = res
+    case 'updateCache': {
+      const promise = update_cache(...input.args)
+      if (promise) {
+        promise.then(res => {
+          console.log(`updateCache: done`)
+          output.data = { cacheRefreshed: true }
+          postMessage(output)
+        })
+      } else {
+        output.data = { cacheRefreshed: false }
         postMessage(output)
-      })
+      }
       break
+    }
   }
 })
