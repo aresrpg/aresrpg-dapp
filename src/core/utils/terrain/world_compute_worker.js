6b68eace855e1e32aadd9f31f5637cf433e70852
etienne <em85@pm.me>
2024-07-31 15:46:51 +0000

    feat: minimalist working version with main feats supported (LOD, trees, worker, cache, ..)

--- src/core/utils/terrain/world_compute_worker.js
+++ src/core/utils/terrain/world_compute_worker.js
@@ -1,20 +1,20 @@
 import {
   Biome,
   Heightmap,
-  PatchCache,
-  PatchBaseCache,
+  BlocksPatch,
+  WorldCompute,
+  WorldApiName,
 } from '@aresrpg/aresrpg-world'
+import { Vector3 } from 'three'
 
 import {
   biome_mapping_conf,
   sea_level,
-  world_cache_pow_limit,
   world_patch_size,
 } from './world_settings.js'
 
-const init_cache = () => {
-  PatchCache.patchSize = world_patch_size
-  PatchBaseCache.cachePowRadius = 1
+const init_world = () => {
+  BlocksPatch.patchSize = world_patch_size
   Heightmap.instance.heightmap.params.spreading = 0.42 // (1.42 - 1)
   Heightmap.instance.heightmap.sampling.harmonicsCount = 6
   Heightmap.instance.amplitude.sampling.seed = 'amplitude_mod'
@@ -23,7 +23,7 @@ const init_cache = () => {
   Biome.instance.params.seaLevel = sea_level
 }
 
-init_cache()
+init_world()
 
 addEventListener('error', e => {
   console.error(e)
@@ -39,29 +39,16 @@ addEventListener('message', ({ data: input }) => {
   const output = {
     id: input.id,
   }
-  switch (input.api) {
-    // case 'getBlock':
-    //   output.data = get_block(...input.args)
-    //   postMessage(output)
-    //   break
-    case 'updateCache': {
-      const on_cache_sync = blocks_patch => {
-        // postMessage(blocks_patch)
-      }
-      PatchBaseCache.updateCache(
-        input.args[0],
-        on_cache_sync,
-        input.args[1],
-      ).then(res => {
-        if (res) {
-          if (PatchBaseCache.cachePowRadius < world_cache_pow_limit) {
-            PatchBaseCache.cachePowRadius += 1
-          }
-        }
-        output.data = res
-        postMessage(output)
-      })
-      break
-    }
+  const { apiName: api_name, args } = input
+  if (
+    [
+      WorldApiName.GroundBlockCompute,
+      WorldApiName.OvergroundBlocksCompute,
+    ].includes(api_name)
+  ) {
+    args[0] = new Vector3(...Object.values(args[0]))
   }
+  const obj_stub = WorldCompute[api_name](...args)
+  output.data = obj_stub
+  postMessage(output)
 })
