7ce0329b530c97cc606522c02248e85afff8d1bb
etienne <em85@pm.me>
2024-08-02 13:17:43 +0000

    fix: decrease default patch cache size

--- src/core/utils/terrain/world_settings.js
+++ src/core/utils/terrain/world_settings.js
@@ -3,7 +3,7 @@ import { BlockType } from '@aresrpg/aresrpg-world'
 // constants
 export const world_patch_size = Math.pow(2, 6)
 // max cache radius as a power of two
-export const world_cache_pow_limit = 3 // 4 => 16 patches radius
+export const world_cache_pow_limit = 2 // 4 => 16 patches radius
 export const sea_level = 76
 
 export const blocks_colors = {
