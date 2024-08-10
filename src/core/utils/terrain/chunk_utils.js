4c5f1e99144b605c66b26ffbbdd0a4b52af84e70
etienne <em85@pm.me>
2024-08-10 16:30:18 +0000

    feat: chunk utils refactor + fix chunk ids range

--- src/core/utils/terrain/chunk_utils.js
+++ src/core/utils/terrain/chunk_utils.js
@@ -1,69 +1,73 @@
 import { voxelmapDataPacking } from '@aresrpg/aresrpg-engine'
-import { BlockType, WorldCache } from '@aresrpg/aresrpg-world'
-import { Vector3, MathUtils, Box3 } from 'three'
+import { BlockType, WorldCache, WorldUtils } from '@aresrpg/aresrpg-world'
+import { Vector3, MathUtils, Box3, Vector2 } from 'three'
 
 import { world_patch_size } from './world_settings.js'
 
 const dbg_highlight_patch_borders = false // debug only
 
 const write_chunk_blocks = (
-  chunk,
-  block_pos,
+  chunk_data,
+  chunk_bbox,
+  block_local_pos,
   ground_type,
   buffer_over = [],
 ) => {
-  const { bbox, cache } = chunk
-  const dimensions = bbox.getSize(new Vector3())
-  const patch_size = dimensions.x
+  const chunk_size = Math.round(Math.pow(chunk_data.length, 1 / 3))
+
+  let written_blocks_count = 0
 
   const level = MathUtils.clamp(
-    block_pos.y + buffer_over.length,
-    bbox.min.y,
-    bbox.max.y,
+    block_local_pos.y + buffer_over.length,
+    chunk_bbox.min.y,
+    chunk_bbox.max.y,
   )
-  let buff_index = Math.max(level - block_pos.y, 0)
-  let h = level - bbox.min.y // local height
+  let buff_index = Math.max(level - block_local_pos.y, 0)
+  let h = level - chunk_bbox.min.y // local height
   // debug_mode && is_edge(local_pos.z, local_pos.x, h, patch_size - 2)
   //   ? BlockType.SAND
   //   : block_cache.type
 
   while (h >= 0) {
-    const cache_index =
-      block_pos.z * Math.pow(patch_size, 2) + h * patch_size + block_pos.x
+    const blocks_index =
+      block_local_pos.z * Math.pow(chunk_size, 2) +
+      h * chunk_size +
+      block_local_pos.x
     const block_type = buff_index > 0 ? buffer_over[buff_index] : ground_type
     const skip =
       buff_index > 0 &&
-      cache[cache_index] !== undefined &&
+      chunk_data[blocks_index] !== undefined &&
       !buffer_over[buff_index]
     if (!skip) {
-      cache[cache_index] = block_type
+      chunk_data[blocks_index] = block_type
         ? voxelmapDataPacking.encode(false, block_type)
         : voxelmapDataPacking.encodeEmpty()
+      block_type && written_blocks_count++
     }
     buff_index--
     h--
   }
+  return written_blocks_count
 }
 
-const edges_blocks_pass = chunk => {
-  const { bbox } = chunk
-  const dimensions = bbox.getSize(new Vector3())
+const edges_blocks_pass = (chunk_data, chunk_box) => {
+  const dimensions = chunk_box.getSize(new Vector3())
   const patch_size = dimensions.x
   for (let i = 0; i < patch_size; i++) {
     const xmin = {
-      global_pos: new Vector3(bbox.min.x, 0, bbox.min.z + i),
+      global_pos: new Vector3(chunk_box.min.x, 0, chunk_box.min.z + i),
       local_pos: new Vector3(0, 0, i),
     }
     const xmax = {
-      global_pos: new Vector3(bbox.max.x, 0, bbox.min.z + i),
+      global_pos: new Vector3(chunk_box.max.x, 0, chunk_box.min.z + i),
       local_pos: new Vector3(patch_size - 1, 0, i),
     }
     const zmin = {
-      global_pos: new Vector3(bbox.min.x + i, 0, bbox.min.z),
+      global_pos: new Vector3(chunk_box.min.x + i, 0, chunk_box.min.z),
       local_pos: new Vector3(i, 0, 0),
     }
     const zmax = {
-      global_pos: new Vector3(bbox.min.x + i, 0, bbox.max.z),
+      global_pos: new Vector3(chunk_box.min.x + i, 0, chunk_box.max.z),
       local_pos: new Vector3(i, 0, patch_size - 1),
     }
     const edges = [xmin, zmin, xmax, zmax]
@@ -74,7 +78,12 @@ const edges_blocks_pass = chunk => {
       if (block_data) {
         const block_local_pos = edge.local_pos.clone()
         block_local_pos.y = block_data.pos.y
-        write_chunk_blocks(chunk, block_local_pos, block_data.type)
+        write_chunk_blocks(
+          chunk_data,
+          chunk_box,
+          block_local_pos,
+          block_data.type,
+        )
       } else {
         // console.log(bbox, edges, patch_size)
         // console.log('missing block: ', edge.global_pos)
@@ -83,38 +92,45 @@ const edges_blocks_pass = chunk => {
   }
 }
 
-const ground_blocks_pass = (patch, chunk) => {
-  const iter = patch.iterBlocks(true)
-  let res = iter.next()
-  while (!res.done) {
-    const block_data = res.value
-    const block_pos = block_data.pos.clone()
-    block_pos.x += 1
-    block_pos.z += 1
+const fill_ground_data = (blocks_container, chunk_data, chunk_box) => {
+  let written_blocks_count = 0
+  const blocks_iter = blocks_container.iterBlocks(true, false)
+  for (const block of blocks_iter) {
+    const block_local_pos = block.pos
+    block_local_pos.x += 1
+    // block_local_pos.y = patch.bbox.max.y
+    block_local_pos.z += 1
     let border_type
     if (dbg_highlight_patch_borders) {
-      if (block_pos.x === 1 || block_pos.z === 1) {
-        border_type = patch.isTransitionPatch ? BlockType.SAND : BlockType.WATER
-        border_type = patch.isBiomeTransition ? BlockType.MUD : border_type
+      if (block_local_pos.x === 1 || block_local_pos.z === 1) {
+        // border_type = patch.isTransitionPatch ? BlockType.SAND : BlockType.WATER
+        // border_type = patch.isBiomeTransition ? BlockType.MUD : border_type
+        border_type = BlockType.SAND
       }
     }
-    const block_type = border_type || block_data.type
-    write_chunk_blocks(chunk, block_pos, block_type)
-    res = iter.next()
+    const block_type = border_type || block.type
+    written_blocks_count += write_chunk_blocks(
+      chunk_data,
+      chunk_box,
+      block_local_pos,
+      block_type,
+    )
   }
+  return written_blocks_count
 }
 
-const entities_blocks_pass = (patch, chunk) => {
-  const patch_bis = WorldCache.getPatch(patch.bbox.getCenter(new Vector3()))
-  const patch_start = patch.bbox.min
-
-  for (const entity_chunk of patch.entitiesChunks) {
-    const { min, max } = entity_chunk.bbox
-    const bmin = new Vector3(...Object.values(min))
-    const bmax = new Vector3(...Object.values(max))
-    const entity_bbox = new Box3(bmin, bmax)
-    const blocks_iter = patch.getBlocks(entity_chunk.bbox, true)
+const fill_entities_data = (blocks_container, chunk_data, chunk_box) => {
+  let written_blocks_count = 0
+  // iter over container entities
+  for (const entity_chunk of blocks_container.entitiesChunks) {
+    // const { min, max } = entity_chunk.bbox
+    // const bmin = new Vector3(...Object.values(min))
+    // const bmax = new Vector3(...Object.values(max))
+    // const entity_bbox = new Box3(bmin, bmax)
+    // find overlapping blocks between entity and container
+    const blocks_iter = blocks_container.getBlocks(entity_chunk.bbox, true)
     let chunk_index = 0
+    // iter over entity blocks
     for (const block of blocks_iter) {
       const buffer_str = entity_chunk.data[chunk_index]
       const buffer =
@@ -124,33 +140,46 @@ const entities_blocks_pass = (patch, chunk) => {
         block.buffer = buffer
         block.localPos.x += 1
         block.localPos.z += 1
-        bmin.y = block.localPos.y
-        write_chunk_blocks(chunk, block.localPos, block.type, block.buffer)
+        // bmin.y = block.localPos.y
+        written_blocks_count += write_chunk_blocks(
+          chunk_data,
+          chunk_box,
+          block.localPos,
+          block.type,
+          block.buffer,
+        )
       }
       chunk_index++
     }
   }
-
-  // const buff_iter = patch_bis.overBlocksIter()
-  // for (const blk of buff_iter) {
-  //   blk.localPos.x += 1
-  //   blk.localPos.z += 1
-  //   write_chunk_blocks(chunk, blk.localPos, blk.type, blk.buffer)
-  // }
+  return written_blocks_count
 }
 
-export function fill_chunk_from_patch(patch, chunk_bbox) {
-  const dimensions = chunk_bbox.getSize(new Vector3())
-  const cache = new Uint16Array(dimensions.x * dimensions.y * dimensions.z)
-  const chunk = { bbox: chunk_bbox, cache }
+export const get_chunk_bbox = chunk_id => {
+  const bmin = chunk_id.clone().multiplyScalar(world_patch_size)
+  const bmax = chunk_id.clone().addScalar(1).multiplyScalar(world_patch_size)
+  const chunk_bbox = new Box3(bmin, bmax)
+  chunk_bbox.expandByScalar(1)
+  return chunk_bbox
+}
+
+export function make_chunk(blocks_container, chunk_id) {
+  const chunk_box = get_chunk_bbox(chunk_id)
+  const final_chunk = make_custom_chunk(blocks_container, chunk_box)
+  final_chunk.id = chunk_id
+  return final_chunk
+}
+
+export function make_custom_chunk(blocks_container, chunk_box) {
+  const chunk_dims = chunk_box.getSize(new Vector3())
+  const chunk_data = new Uint16Array(chunk_dims.x * chunk_dims.y * chunk_dims.z)
+  let total_written_blocks_count = 0
   // const debug_mode = true
 
   // const is_edge = (row, col, h, patch_size) =>
   //   row === 1 || row === patch_size || col === 1 || col === patch_size
   // || h === 1
   // || h === patch_size - 2
-
-  let is_empty = true
 
   // const patch = PatchBlocksCache.instances.find(
   //   patch =>
@@ -162,58 +191,41 @@ export function fill_chunk_from_patch(patch, chunk_bbox) {
   // )
 
   // multi-pass chunk filling
-  if (patch) {
+  if (blocks_container) {
     // ground pass
-    ground_blocks_pass(patch, chunk)
+    total_written_blocks_count += fill_ground_data(
+      blocks_container,
+      chunk_data,
+      chunk_box,
+    )
     // overground entities pass
-    entities_blocks_pass(patch, chunk)
+    total_written_blocks_count += fill_entities_data(
+      blocks_container,
+      chunk_data,
+      chunk_box,
+    )
     // extra blocks at edges from adjacent patches
-    edges_blocks_pass(chunk)
-    is_empty = false
+    edges_blocks_pass(chunk_data, chunk_box)
   }
-  return chunk.cache
+  // const size = Math.round(Math.pow(chunk.data.length, 1 / 3))
+  // const dimensions = new Vector3(size, size, size)
+  const chunk = {
+    data: chunk_data,
+    size: chunk_dims,
+    isEmpty: total_written_blocks_count === 0,
+  }
+  return chunk
 }
 
-export function get_patch_chunk_ids(patch_key) {
+export function gen_chunk_ids(patch, ymin, ymax) {
   const chunk_ids = []
-  const patch = WorldCache.patchLookupIndex[patch_key]
   if (patch) {
-    for (let i = 6; i >= 0; i--) {
-      const chunk_coords = new Vector3(patch.coords.x, i, patch.coords.y)
+    for (let y = ymax; y >= ymin; y--) {
+      const chunk_coords = WorldUtils.asVect3(patch.coords, y)
       chunk_ids.push(chunk_coords)
     }
   }
   return chunk_ids
-}
-
-export function get_chunks_ids(patch_keys) {
-  // build chunk index
-  const patch_chunks_index = {}
-  // Object.values(WorldCache.patchLookupIndex)
-  const chunks_indices = patch_keys
-    .map(patch_key => get_patch_chunk_ids(patch_key))
-    .flat()
-  return chunks_indices
-}
-
-export function get_patch_chunks(patch_key) {
-  const patch = WorldCache.patchLookupIndex[patch_key]
-  const chunks_ids = get_patch_chunk_ids(patch_key)
-  const chunks = chunks_ids.map(chunk_coords => {
-    const bmin = chunk_coords.clone().multiplyScalar(world_patch_size)
-    const bmax = chunk_coords
-      .clone()
-      .addScalar(1)
-      .multiplyScalar(world_patch_size)
-    const chunk_bbox = new Box3(bmin, bmax)
-    chunk_bbox.expandByScalar(1)
-    const data = fill_chunk_from_patch(patch, chunk_bbox)
-    const size = Math.round(Math.pow(data.length, 1 / 3))
-    const dimensions = new Vector3(size, size, size)
-    const chunk = { id: chunk_coords, data, size: dimensions, isEmpty: false }
-    return chunk
-  })
-  return chunks
 }
 
 function get_ground_block({ x, z }, entity_height) {
