ec393adfb1f0493dce0b57aa40897426a7dfa2a1
Sceat <fetch@sceat.xyz>
2024-10-13 22:38:04 +0800

    feat: improve toasts

--- src/env.js
+++ src/env.js
@@ -1,6 +1,5 @@
 const {
   VITE_SUI_RPC = 'https://fullnode.testnet.sui.io',
-  VITE_SUI_WSS = 'wss://fullnode.testnet.sui.io',
   VITE_SERVER_URL,
   VITE_SPONSOR_URL = 'https://testnet-sponsor.aresrpg.world',
   VITE_INDEXER_URL = 'https://testnet-indexer.aresrpg.world',
@@ -11,7 +10,6 @@ const {
 export const NETWORK = VITE_NETWORK
 export {
   VITE_SUI_RPC,
-  VITE_SUI_WSS,
   VITE_SERVER_URL,
   VITE_ENOKI_KEY,
   VITE_SPONSOR_URL,
