12684316c3b69e3856271b9c70a822436996e67b
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-10-14 22:41:00 +0200

    tmp

--- src/core/modules/game_camera.js
+++ src/core/modules/game_camera.js
@@ -9,6 +9,9 @@ import { context, current_three_character } from '../game/game.js'
 import { sea_level } from '../utils/terrain/world_settings.js'
 
 import { is_hovering_mob_group } from './player_entities_interract.js'
+import { Vector3 } from 'three'
+import { GameCamera } from '../game/rendering/camera.js'
+import { VoxelmapCollisions } from '@aresrpg/aresrpg-engine'
 
 const CAMERA_MIN_DISTANCE = 0.001
 const CAMERA_DISTANCE_STEP = 1
@@ -16,8 +19,44 @@ const CAMERA_MAX_DISTANCE = 50
 
 /** @type {Type.Module} */
 export default function () {
+  let wanted_distance = 0;
+  let last_intersection_distance = Infinity;
+
+  function compute_intersection_distance(/** @type Vector3 */ focus_point, /** @type GameCamera */ camera, /** @type VoxelmapCollisions */ collisions) {
+    const camera_right = camera.right;
+    const camera_up = new Vector3().crossVectors(camera_right, camera.forward);
+    const camera_position = camera.position;
+  
+    const intersections = [];
+  
+    const focus_point_with_offset = new Vector3();
+    const focus_to_camera = new Vector3();
+    for (let iX = -1; iX <= 1; iX += 0.2) {
+      for (let iY = -1; iY <= 1; iY += 0.2) {
+        focus_point_with_offset.copy(focus_point).addScaledVector(camera_right, iX).addScaledVector(camera_up, iY);
+        focus_to_camera.subVectors(camera_position, focus_point_with_offset);
+        const intersection = collisions.rayCast(focus_point_with_offset, focus_to_camera);
+        if (intersection) {
+          intersections.push(intersection);
+        }
+      }
+    }
+
+    if (intersections.length === 0) {
+      return null;
+    }
+
+    let max_intersection_distance = -Infinity;
+    for (const intersection of intersections) {
+      if (intersection.distance > max_intersection_distance) {
+        max_intersection_distance = intersection.distance;
+      }
+    }
+    return max_intersection_distance;
+  }
+
   return {
-    tick(state, { dispatch, camera }, delta) {
+    tick(state, { dispatch, camera, physics }, delta) {
       const { camera_controls } = camera
 
       const player = current_three_character(state)
@@ -30,13 +69,29 @@ export default function () {
 
         // Set the perspective camera position to follow the player
         const center_camera_on_head =
-          1 - smootherstep(camera_controls.distance, 0, 10)
+          0 // 1 - smootherstep(camera_controls.distance, 0, 10)
         const head_height = 1
         const y_shift = state.current_fight
           ? -5
           : head_height * center_camera_on_head
         camera_controls.moveTo(x, y + y_shift, z, true)
         camera_controls.setTarget(x, y + y_shift, z, true)
+
+        // const intersection_distance = compute_intersection_distance(new Vector3(x, y + y_shift, z), camera, physics.voxelmap_collisions)
+        const camera_focus = new Vector3(x, y + y_shift, z);
+        const max_intersection = camera_focus.clone().addScaledVector(
+          new Vector3().subVectors(camera.position, camera_focus).normalize(),
+          CAMERA_MAX_DISTANCE + 1);
+        const intersection = physics.voxelmap_collisions.rayCast(player.position, max_intersection);
+        const intersection_distance = intersection?.distance ?? Infinity;
+
+        const current_distance = camera_controls.distance;
+        if (intersection_distance <= current_distance) {
+          camera_controls.distance = intersection_distance;
+        } else if (intersection_distance > last_intersection_distance + 1e-5) {
+          camera_controls.dollyTo(Math.min(intersection_distance, wanted_distance));
+        }
+        last_intersection_distance = intersection_distance;
 
         if (typeof player.object3d !== 'undefined') {
           player.object3d.visible = camera_controls.distance > 0.75
@@ -69,6 +124,10 @@ export default function () {
     observe({ events, camera, renderer, signal }) {
       const { camera_controls } = camera
 
+      // setInterval(() => {
+      //   camera_controls.dollyTo(10 * (0.5 + 0.5 * Math.cos(performance.now() / 1000)))
+      // }, 50)
+
       camera_controls.dollyDragInverted = true
       camera_controls.dollyToCursor = true
       camera_controls.maxDistance = CAMERA_MAX_DISTANCE
@@ -78,9 +137,9 @@ export default function () {
       camera_controls.rotate(0, 1)
 
       const set_distance = distance => {
-        distance = clamp(distance, CAMERA_MIN_DISTANCE, CAMERA_MAX_DISTANCE)
-        distance = Math.round(distance)
-        camera_controls.dollyTo(distance, true)
+        wanted_distance = clamp(distance, CAMERA_MIN_DISTANCE, CAMERA_MAX_DISTANCE)
+        wanted_distance = Math.round(distance)
+        camera_controls.dollyTo(wanted_distance, true)
       }
 
       const on_mouse_down = () => {
