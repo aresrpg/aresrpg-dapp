ec7ed8504f690256cbda0db7daa78d5a5cd2bbcf
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-12-01 18:36:31 +0100

    refactor: factorize code

---
+++ src/core/game/rendering/utils.js
@@ -0,0 +1,20 @@
+import {
+  BufferGeometry,
+  Float32BufferAttribute,
+  Mesh,
+  Uint16BufferAttribute,
+} from 'three'
+
+function create_fullscreen_quad() {
+  const quad_geometry = new BufferGeometry()
+  quad_geometry.setAttribute(
+    'aCorner',
+    new Float32BufferAttribute([-1, +1, +1, +1, -1, -1, +1, -1], 2),
+  )
+  quad_geometry.setIndex(new Uint16BufferAttribute([0, 2, 1, 2, 3, 1], 1))
+  const fullscreen_quad = new Mesh(quad_geometry)
+  fullscreen_quad.frustumCulled = false
+  return fullscreen_quad
+}
+
+export { create_fullscreen_quad }
