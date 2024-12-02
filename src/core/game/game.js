55bd1875320a7d9ebdb43a3b6b1b625117c813fd
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-12-02 12:31:42 +0100

    feat: fog is now 3d

--- src/core/game/game.js
+++ src/core/game/game.js
@@ -163,7 +163,7 @@ export const INITIAL_STATE = {
       color: new Color(0x29b6f6),
     },
 
-    view_distance: 100,
+    view_distance: 50,
 
     camera: {
       is_free: false,
@@ -180,7 +180,7 @@ export const INITIAL_STATE = {
       },
 
       godrays_pass: {
-        enabled: true,
+        enabled: false,
         light_size: 0.0025,
         max_intensity: 1,
         exposure: 0.3,
@@ -368,7 +368,7 @@ const camera = new PerspectiveCamera(
   70, // Field of view
   window.innerWidth / window.innerHeight, // Aspect ratio
   0.1, // Near clipping plane
-  3000, // Far clipping plane
+  500, // Far clipping plane
 )
 camera.layers.enableAll()
 
