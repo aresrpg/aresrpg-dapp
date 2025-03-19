d80ba65279a15cd35b891a6b6e588196df736c5b
Sceat <fetch@sceat.xyz>
2025-03-19 23:19:30 +0800

    feat: update music + worker names

--- src/core/worker/workers.js
+++ src/core/worker/workers.js
@@ -4,18 +4,18 @@ import { WorkerPool } from '@aresrpg/aresrpg-world/workerpool'
 
 import logger from '../../logger.js'
 
-async function create_worker_pool(size = 1) {
+async function create_worker_pool(name, size = 1) {
   logger.INTERNAL(`create worker_pool of size: ${size}`)
   const pool = new WorkerPool()
-  await pool.initPoolEnv(size, world_settings, worker_url)
+  await pool.initPoolEnv(size, world_settings, worker_url, name)
   return pool
 }
 
 const [LOD_WORKER_POOL, TERRAIN_WORKER_POOL, DEFAULT_WORKER_POOL] =
   await Promise.all([
-    create_worker_pool(),
-    create_worker_pool(navigator.hardwareConcurrency),
-    create_worker_pool(),
+    create_worker_pool('lod-worker', 4),
+    create_worker_pool('chunks-worker', navigator.hardwareConcurrency),
+    create_worker_pool('default-worker'),
   ])
 
 export { LOD_WORKER_POOL, TERRAIN_WORKER_POOL, DEFAULT_WORKER_POOL }
