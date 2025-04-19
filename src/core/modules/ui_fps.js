aa9c1cd927b6ff5b14bb02cca0b3c6d08f2cb129
StonyTV <j.dathueyt@gmail.com>
2025-04-19 11:35:55 +0200

    feat: Add stats.js for fps only

--- src/core/modules/ui_fps.js
+++ src/core/modules/ui_fps.js
@@ -44,8 +44,9 @@ export default function () {
 
   function show_stats(show) {
     if (show) {
-      document.body.appendChild(stats_entities.dom)
       document.body.appendChild(stats_fps.dom)
+      if (VITE_DEMO_MODE) return
+      document.body.appendChild(stats_entities.dom)
       document.body.appendChild(stats_memory.dom)
       document.body.appendChild(stats_mesh.dom)
       document.body.appendChild(stats_draw_calls.dom)
@@ -74,8 +75,8 @@ export default function () {
 
   return {
     tick(state, { scene, renderer }) {
-      stats_entities.update()
       stats_fps.update()
+      stats_entities.update()
       stats_memory.update()
       mesh_panel.update(count_meshes(scene), 1000) // 1000 is an arbitrary max value
       draw_calls_panel.update(renderer.info.render.calls, 5000) // 5000 is an arbitrary max value for draw calls
@@ -106,7 +107,6 @@ export default function () {
         })
 
       on_game_show(() => {
-        if (VITE_DEMO_MODE) return
         show_stats(show_fps)
       })
 
