8eef26efaaedb670c760480b8c23efb6032463f0
default <em85@pm.me>
2025-04-27 18:18:19 +0000

    fix: worker naming

--- src/core/worker/workers.js
+++ src/core/worker/workers.js
@@ -6,22 +6,27 @@ import logger from '../../logger.js'
 
 async function create_worker_pool(name, size = 1) {
   logger.INTERNAL(`create worker_pool of size: ${size}`)
-  const pool = new WorkerPool()
+  const pool = new WorkerPool(name)
   // built-in workers (if not working use external version below)
   await pool.initPoolEnv(size, world_settings)
   // external workers (if builtin are failing)
-  // await pool.initPoolEnv(size, world_settings, () => new Worker({ name }))
+  // const worker_opt = { name: name + '-worker' }
+  // await pool.initPoolEnv(
+  //   size,
+  //   world_settings,
+  //   () => new Worker(worker_opt),
+  // )
   return pool
 }
 
 const [LOD_WORKER_POOL, TERRAIN_WORKER_POOL, DEFAULT_WORKER_POOL] =
   await Promise.all([
-    create_worker_pool('lod-worker'),
+    create_worker_pool('lod'),
     create_worker_pool(
-      'chunks-worker',
+      'chunks',
       Math.min(navigator.hardwareConcurrency, 4), // limit workers to avoid eating CPU resources, and timeout bug
     ),
-    create_worker_pool('default-worker'),
+    create_worker_pool('default'),
   ])
 
 export { LOD_WORKER_POOL, TERRAIN_WORKER_POOL, DEFAULT_WORKER_POOL }
